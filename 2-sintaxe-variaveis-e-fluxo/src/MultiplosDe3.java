1019f5fb2fb4fbbe7619f11ed7fa6cb5aed6a255
fjp-j <fjp-j@192.168.0.4>
2020-09-06 21:34:51 -0300

    sintaxe-variaveis-e-fluxo

---
+++ 2-sintaxe-variaveis-e-fluxo/src/MultiplosDe3.java
@@ -0,0 +1,9 @@
+
+public class MultiplosDe3 {
+	public static void main(String[] args) {
+		for (int contador = 3; contador <= 100; contador = contador + 3) {
+			System.out.println(contador);
+		}
+	}
+
+}
